1e08670a01a7fc966cce1f2e4275420cb3019b17
Mark Wlodawski <imakethesites@gmail.com>
2023-02-10 12:10:02 -0500

    Initial commit

---
+++ client/src/components/JobBoard.js
@@ -0,0 +1,27 @@
+import JobList from "./JobList";
+import { getJobs } from "./graphql/queries";
+import { useEffect, useState } from "react";
+
+function JobBoard() {
+  const [jobs, setJobs] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getJobs()
+      .then(setJobs)
+      .catch((err) => setError(true));
+  }, []);
+
+  console.log("[JobBoard] jobs:", jobs);
+  if (error) {
+    return <p>Sorry, something went wrong</p>;
+  }
+  return (
+    <div>
+      <h1 className="title">Job Board</h1>
+      <JobList jobs={jobs} />
+    </div>
+  );
+}
+
+export default JobBoard;
