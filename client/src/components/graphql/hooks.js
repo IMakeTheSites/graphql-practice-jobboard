d1b57b994117c32c9b0ea2bb9a9893eb87b591b0
Mark Wlodawski <imakethesites@gmail.com>
2023-02-11 21:45:08 -0500

    useCreateJob hook

--- client/src/components/graphql/hooks.js
+++ client/src/components/graphql/hooks.js
@@ -1,5 +1,46 @@
-import { useQuery } from "@apollo/client";
-import { COMPANY_QUERY, JOBS_QUERY, JOB_QUERY } from "./queries";
+import { useMutation, useQuery } from "@apollo/client";
+import {
+  COMPANY_QUERY,
+  CREATE_JOB_MUTATION,
+  JOBS_QUERY,
+  JOB_QUERY,
+} from "./queries";
+import { getAccessToken } from "../../auth";
+
+export function useCompany(id) {
+  const { data, loading, error } = useQuery(COMPANY_QUERY, {
+    variables: { id },
+  });
+  return {
+    company: data?.company,
+    loading,
+    error: Boolean(error),
+  };
+}
+
+export function useCreateJob() {
+  const [mutate, { loading, error }] = useMutation(CREATE_JOB_MUTATION);
+  return {
+    createJob: async (title, description) => {
+      const {
+        data: { job },
+      } = await mutate({
+        variables: { input: { title, description } },
+        context: { headers: { Authorization: "Bearer " + getAccessToken() } },
+        update: (cache, { data: { job } }) => {
+          cache.writeQuery({
+            query: JOB_QUERY,
+            variables: { id: job.id },
+            data: { job },
+          });
+        },
+      });
+      return job;
+    },
+    loading,
+    error: Boolean(error),
+  };
+}
 
 export function useJob(id) {
   const { data, loading, error } = useQuery(JOB_QUERY, {
@@ -18,17 +59,6 @@ export function useJobs() {
   });
   return {
     jobs: data?.jobs,
-    loading,
-    error: Boolean(error),
-  };
-}
-
-export function useCompany(id) {
-  const { data, loading, error } = useQuery(COMPANY_QUERY, {
-    variables: { id },
-  });
-  return {
-    company: data?.company,
     loading,
     error: Boolean(error),
   };
