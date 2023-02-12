47d47051190b394fdae0990de908bdab24279a3f
Mark Wlodawski <imakethesites@gmail.com>
2023-02-11 19:27:12 -0500

    useQuery with variables

--- client/src/components/JobDetail.js
+++ client/src/components/JobDetail.js
@@ -1,14 +1,10 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { getJob } from "./graphql/queries";
+import { useJob } from "./graphql/hooks";
 
 function JobDetail() {
-  const [job, setJob] = useState(null);
   const { jobId } = useParams();
-  useEffect(() => {
-    getJob(jobId).then(setJob);
-  }, []);
+  const { job, loading } = useJob(jobId);
 
   console.log("[JobDetail] job:", job);
   if (!job) {
